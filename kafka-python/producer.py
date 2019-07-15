946dc05cc3e4f8a41f8d6ab859ec68eb8dc7da72
Dinesh <janepelladinesh97@gmail.com>
2019-07-15 10:29:53 +0530

    Add files via upload

---
+++ kafka-python/producer.py
@@ -0,0 +1,48 @@
+import threading, logging, time
+import multiprocessing
+
+from kafka import  KafkaProducer
+
+
+class Producer(threading.Thread):
+    def __init__(self):
+        threading.Thread.__init__(self)
+        self.stop_event = threading.Event()
+        
+    def stop(self):
+        self.stop_event.set()
+
+    def run(self):
+        producer = KafkaProducer(bootstrap_servers='10.128.0.202:9092')
+
+     
+            producer.send('test', b"big boss season 3 started")
+            time.sleep(1)
+
+        producer.close()
+
+        
+        
+def main():
+    tasks = [
+        Producer()
+    ]
+
+    for t in tasks:
+        t.start()
+
+    time.sleep(10)
+    
+    for task in tasks:
+        task.stop()
+
+    for task in tasks:
+        task.join()
+        
+        
+if __name__ == "__main__":
+    logging.basicConfig(
+        format='%(asctime)s.%(msecs)s:%(name)s:%(thread)d:%(levelname)s:%(process)d:%(message)s',
+        level=logging.INFO
+        )
+    main()
